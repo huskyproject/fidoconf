5955496a0a2cb32d0fc2912a84cb07c951debcb1
mt <>
1998-12-16 22:58:48 +0000

    changed fidoconfig licence from gpl to lgpl

--- fconf2aquaed.c
+++ fconf2aquaed.c
@@ -1,3 +1,34 @@
+/******************************************************************************
+ * FIDOCONFIG --- library for fidonet configs
+ ******************************************************************************
+ * Copyright (C) 1998
+ *  
+ * Matthias Tichy
+ *
+ * Fido:     2:2433/1245 2:2433/1247 2:2432/601.29
+ * Internet: mtt@tichy.de
+ *
+ * Grimmestr. 12         Buchholzer Weg 4
+ * 33098 Paderborn       40472 Duesseldorf
+ * Germany               Germany
+ *
+ * This file is part of FIDOCONFIG.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; see file COPYING. If not, write to the Free
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *****************************************************************************/
+
 #include <stdlib.h>
 
 #include "fidoconfig.h"
