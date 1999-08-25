bbe58e7cda2310d846134095479c7044a44cb85d
sms <>
1999-08-25 21:42:01 +0000

    sms: + did some work on dumpcfg.c + changes LOPT in makefile.lnx

--- dump.c
+++ dump.c
@@ -13,12 +13,13 @@ void dumpHeader(s_fidoconfig *config, FILE *f)
 {
   fprintf(f, "# This config was created from a memory config by fidoconfig dumpConfigToFile()!\n\n"); 
 
-  fprintf(f, "Version %u.%02u\n", config->cfgVersionMajor, config->cfgVersionMinor);
-  dumpString(f, "Name %s\n", config->name);
-  dumpString(f, "Sysop %s\n", config->sysop);
-  dumpString(f, "Location %s\n\n", config->location);
+  fprintf(f,    "Version             %u.%02u\n", config->cfgVersionMajor,
+	  config->cfgVersionMinor);
+  dumpString(f, "Name                %s\n", config->name);
+  dumpString(f, "Sysop               %s\n", config->sysop);
+  dumpString(f, "Location            %s\n\n", config->location);
   
-  dumpString(f, "LogLevels %s\n\n", config->loglevels);
+  dumpString(f, "LogLevels           %s\n\n", config->loglevels);
   if (config->logEchoToScreen != 0) fprintf(f, "logEchoToScreen\n");
   
   fprintf(f, "\n");
@@ -29,7 +30,7 @@ void dumpAddrs(s_fidoconfig *config, FILE *f)
   int i;
 
   for (i = 0; i < config->addrCount; i++)
-    fprintf(f, "Address %s\n", aka2str(config->addr[i]));
+    fprintf(f, "Address             %s\n", aka2str(config->addr[i]));
   fprintf(f, "\n");
 }
 
@@ -45,10 +46,10 @@ void dumpPack(s_fidoconfig *config, FILE *f)
   for (i = 0; i < max; i++)
     {
       if (i < config->packCount)
-	fprintf(f, "Pack %s  %s\n", config->pack[i].packer, config->pack[i].call);
+	fprintf(f, "Pack                %s %s\n", config->pack[i].packer, config->pack[i].call);
       if (i < config->unpackCount)
 	{
-	  fprintf(f, "Unpack \"%s\" %u", config->unpack[i].call, config->unpack[i].offset);
+	  fprintf(f, "Unpack              \"%s\" %u", config->unpack[i].call, config->unpack[i].offset);
 	  for (j = 0; j < config->unpack[i].codeSize; j++)
 	    fprintf(f, "%02x", (int) config->unpack[i].matchCode[j]);
 	  fprintf(f, " ");
@@ -59,7 +60,7 @@ void dumpPack(s_fidoconfig *config, FILE *f)
     }
 
   if (config->separateBundles != 0) fprintf(f, "separateBundles\n");
-  if (config->defarcmailSize != 0) fprintf(f, "defarcmailSize %d\n",
+  if (config->defarcmailSize != 0) fprintf(f, "defarcmailSize      %d\n",
 					  config->defarcmailSize);
   
   fprintf(f, "\n");
@@ -69,35 +70,35 @@ void dumpPaths(s_fidoconfig *config, FILE *f)
 {
   int i;
 
-  dumpString(f, "Inbound %s\n", config->inbound);
-  dumpString(f, "ListInbound %s\n", config->listInbound);
-  dumpString(f, "ProtInbound %s\n", config->protInbound);
-  dumpString(f, "TempInbound %s\n", config->tempInbound);
-  dumpString(f, "LocalInbound %s\n", config->localInbound); 
+  dumpString(f, "Inbound             %s\n", config->inbound);
+  dumpString(f, "ListInbound         %s\n", config->listInbound);
+  dumpString(f, "ProtInbound         %s\n", config->protInbound);
+  dumpString(f, "TempInbound         %s\n", config->tempInbound);
+  dumpString(f, "LocalInbound        %s\n", config->localInbound);
 
-  dumpString(f, "Outbound %s\n", config->outbound);
-  dumpString(f, "TempOutbound %s\n", config->tempOutbound);
+  dumpString(f, "Outbound            %s\n", config->outbound);
+  dumpString(f, "TempOutbound        %s\n", config->tempOutbound);
   
-  dumpString(f, "LogFileDir %s\n", config->logFileDir);
-  dumpString(f, "DupeHistoryDir %s\n", config->dupeHistoryDir);
-  dumpString(f, "MsgBaseDir %s\n", config->msgBaseDir);
-  dumpString(f, "NodelistDir %s\n", config->nodelistDir);
-  dumpString(f, "Magic %s\n", config->magic);
+  dumpString(f, "LogFileDir          %s\n", config->logFileDir);
+  dumpString(f, "DupeHistoryDir      %s\n", config->dupeHistoryDir);
+  dumpString(f, "MsgBaseDir          %s\n", config->msgBaseDir);
+  dumpString(f, "NodelistDir         %s\n", config->nodelistDir);
+  dumpString(f, "Magic               %s\n", config->magic);
   for (i = 0; i < config->publicCount; i++)
     {
-      fprintf(f, "Public %s\n", config->publicDir[i]);
+      fprintf(f, "Public            %s\n", config->publicDir[i]);
     }
-  dumpString(f, "FileAreaBaseDir %s\n", config->fileAreaBaseDir);
-  dumpString(f, "PassFileAreaDir %s\n", config->passFileAreaDir);
+  dumpString(f, "FileAreaBaseDir     %s\n", config->fileAreaBaseDir);
+  dumpString(f, "PassFileAreaDir     %s\n", config->passFileAreaDir);
   
-  dumpString(f, "AreaFixHelp %s\n", config->areafixhelp);
-  dumpString(f, "FileFixHelp %s\n", config->filefixhelp);
-  dumpString(f, "Intab %s\n", config->intab);
-  dumpString(f, "Outtab %s\n", config->outtab);
-  dumpString(f, "EchotossLog %s\n", config->echotosslog);
-  dumpString(f, "ImportLog %s\n", config->importlog);
-  dumpString(f, "LinkWithImportlog %s\n", config->LinkWithImportlog);
-  dumpString(f, "Lockfile %s\n", config->lockfile);
+  dumpString(f, "AreaFixHelp         %s\n", config->areafixhelp);
+  dumpString(f, "FileFixHelp         %s\n", config->filefixhelp);
+  dumpString(f, "Intab               %s\n", config->intab);
+  dumpString(f, "Outtab              %s\n", config->outtab);
+  dumpString(f, "EchotossLog         %s\n", config->echotosslog);
+  dumpString(f, "ImportLog           %s\n", config->importlog);
+  dumpString(f, "LinkWithImportlog   %s\n", config->LinkWithImportlog);
+  dumpString(f, "Lockfile            %s\n", config->lockfile);
   
   fprintf(f, "\n");
 }
@@ -110,104 +111,103 @@ void dumpLinks(s_fidoconfig *config, FILE *f)
   for (i = 0; i < config->linkCount; i++)
     {
       link = config->links[i];
-
-      dumpString(f, "Link %s\n", link.name);
-      fprintf(f, "Aka %s\n", aka2str(link.hisAka));
-      fprintf(f, "ourAka %s\n", aka2str(*link.ourAka));
-      dumpString(f, "Password %s\n", link.defaultPwd);
+      dumpString(f, "Link                %s\n", link.name);
+      fprintf(f, "Aka                 %s\n", aka2str(link.hisAka));
+      fprintf(f, "ourAka              %s\n", aka2str(*link.ourAka));
+      dumpString(f, "Password            %s\n", link.defaultPwd);
       if (stricmp(link.defaultPwd, link.pktPwd)!=0)
-	fprintf(f, "pktPwd %s\n", link.pktPwd);
+	fprintf(f, "pktPwd              %s\n", link.pktPwd);
       if (stricmp(link.defaultPwd, link.ticPwd)!=0)
-	fprintf(f, "ticPwd %s\n", link.ticPwd);
+	fprintf(f, "ticPwd              %s\n", link.ticPwd);
       if (stricmp(link.defaultPwd, link.areaFixPwd)!=0)
-	fprintf(f, "areaFixPwd %s\n", link.areaFixPwd);
+	fprintf(f, "areaFixPwd          %s\n", link.areaFixPwd);
       if (stricmp(link.defaultPwd, link.fileFixPwd)!=0)
-	fprintf(f, "fileFixPwd %s\n", link.fileFixPwd);
+	fprintf(f, "fileFixPwd          %s\n", link.fileFixPwd);
       if (stricmp(link.bbsPwd, link.defaultPwd)!=0)
-	fprintf(f, "bbsPwd %s\n", link.bbsPwd);
-      dumpString(f, "handle %s\n", link.handle);
+	fprintf(f, "bbsPwd              %s\n", link.bbsPwd);
+      dumpString(f, "handle              %s\n", link.handle);
       if (link.autoAreaCreate != 0)
-	fprintf(f, "autoAreaCreate on\n");
+	fprintf(f, "autoAreaCreate      on\n");
       if (link.autoFileCreate != 0)
-	fprintf(f, "autoFileCreate on\n");
+	fprintf(f, "autoFileCreate      on\n");
       if (link.AreaFix != 1)
-	fprintf(f, "areafix off\n");
+	fprintf(f, "areafix             off\n");
       if (link.FileFix != 1)
-	fprintf(f, "fileFix off\n");
+	fprintf(f, "fileFix             off\n");
       if (link.forwardRequests != 0)
-	fprintf(f, "forwardRequests on\n");
+	fprintf(f, "forwardRequests     on\n");
       if (link.fReqFromUpLink != 0)
-	fprintf(f, "fReqFromUpLink on\n");
+	fprintf(f, "fReqFromUpLink      on\n");
 
       switch (link.forwardPkts) {
       case fSecure:
-	fprintf(f, "forwardPkts secure\n");
+	fprintf(f, "forwardPkts         secure\n");
 	break;
       case fOn:
-	fprintf(f, "forwardPkts on\n");
+	fprintf(f, "forwardPkts         on\n");
 	break;
       default:;
       }
       switch (link.allowEmptyPktPwd) {
       case eSecure:
-	fprintf(f, "allowEmptyPktPwd secure\n");
+	fprintf(f, "allowEmptyPktPwd    secure\n");
 	break;
       case eOn:
-	fprintf(f, "allowEmptyPktPwd on\n");
+	fprintf(f, "allowEmptyPktPwd    on\n");
 	break;
       default:;
       }
       
       if (link.packerDef != NULL)
-	fprintf(f, "packer %s\n", link.packerDef->packer);
+	fprintf(f, "packer              %s\n", link.packerDef->packer);
       
       switch (link.echoMailFlavour) {
       case hold:
-	fprintf(f, "echoMailFlavour hold\n");
+	fprintf(f, "echoMailFlavour     hold\n");
 	break;
       case crash:
-	fprintf(f, "echoMailFlavour crash\n");
+	fprintf(f, "echoMailFlavour     crash\n");
 	break;
       case direct:
-	fprintf(f, "echoMailFlavour direct\n");
+	fprintf(f, "echoMailFlavour     direct\n");
 	break;
       case immediate:
-	fprintf(f, "echoMailFlavour immediate\n");
+	fprintf(f, "echoMailFlavour     immediate\n");
 	break;
       default:;
       }
 
-      dumpString(f, "linkGrp %s\n", link.LinkGrp);
-      dumpString(f, "accessGrp %s\n", link.AccessGrp);
-      dumpString(f, "autoAreaCreateFile %s\n", link.autoAreaCreateFile);
-      dumpString(f, "autoFileCreateFile %s\n", link.autoFileCreateFile);
+      dumpString(f, "linkGrp             %s\n", link.LinkGrp);
+      dumpString(f, "accessGrp           %s\n", link.AccessGrp);
+      dumpString(f, "autoAreaCreateFile  %s\n", link.autoAreaCreateFile);
+      dumpString(f, "autoFileCreateFile  %s\n", link.autoFileCreateFile);
       dumpString(f, "autoAreaCreateDefaults %s\n", link.autoAreaCreateDefaults);
       dumpString(f, "autoFileCreateDefaults %s\n", link.autoFileCreateDefaults);
-      dumpString(f, "forwardRequestFile %s\n", link.forwardRequestFile);
-      dumpString(f, "RemoteRobotName %s\n", link.RemoteRobotName);
+      dumpString(f, "forwardRequestFile  %s\n", link.forwardRequestFile);
+      dumpString(f, "RemoteRobotName     %s\n", link.RemoteRobotName);
       if (link.Pause != 0)
-	fprintf(f, "pause %u\n", link.Pause);
+	fprintf(f, "pause\n");
       if (link.autoPause != 0)
-	fprintf(f, "autoPause %u\n", link.autoPause);
+	fprintf(f, "autoPause            %u\n", link.autoPause);
       if (link.level != 0)
-	fprintf(f, "level %u\n", link.level);
+	fprintf(f, "level                %u\n", link.level);
       if (link.arcmailSize != 0)
-	fprintf(f, "arcmailSize %u\n", link.arcmailSize);
+	fprintf(f, "arcmailSize          %u\n", link.arcmailSize);
 
       if (link.export != NULL)
       {
-	if (link.export[0] == 1) fprintf(f, "export on\n");
-	else fprintf(f, "export off\n");
+	if (link.export[0] == 1) fprintf(f, "export              on\n");
+	else fprintf(f, "export              off\n");
       }
       if (link.import != NULL)
       {
-	if (link.import[0] == 1) fprintf(f, "import on\n");
-	else fprintf(f, "import off\n");
+	if (link.import[0] == 1) fprintf(f, "import              on\n");
+	else fprintf(f, "import              off\n");
       }
       if (link.mandatory != NULL)
       {
-	if (link.mandatory[0] == 1) fprintf(f, "mandatory on\n");
-	else fprintf(f, "mandatory off\n");
+	if (link.mandatory[0] == 1) fprintf(f, "mandatory          on\n");
+	else fprintf(f, "mandatory             off\n");
       }
 
       fprintf(f, "\n");
@@ -223,43 +223,42 @@ void dumpRoute(s_route *route, int count, char *prefix, FILE *f)
 
       switch (route[i].flavour) {
       case normal:
-	fprintf(f, "normal ");
+	fprintf(f, "normal    ");
 	break;
       case hold:
-	fprintf(f, "hold ");
+	fprintf(f, "hold      ");
 	break;
       case crash:
-	fprintf(f, "crash ");
+	fprintf(f, "crash     ");
 	break;
       case direct:
-	fprintf(f, "direct ");
+	fprintf(f, "direct    ");
 	break;
       case immediate:
 	fprintf(f, "immediate ");
 	break;
-      default:;
       }
       
       if (route[i].enc != 0)
-	  fprintf(f, "enc ");
-      else fprintf(f, "noenc ");
+	  fprintf(f, "enc       ");
+      else fprintf(f, "noenc     ");
 
-      if (route[i].target != NULL) fprintf(f, "%s ", aka2str(route[i].target->hisAka));
+      if (route[i].target != NULL) fprintf(f, "%-19s ", aka2str(route[i].target->hisAka));
       else
       {
 	  switch(route[i].routeVia)
 	  {
 	  case host:
-	      fprintf(f, "host ");
+	      fprintf(f, "host                ");
               break;
 	  case hub:
-	      fprintf(f, "hub ");
+	      fprintf(f, "hub                 ");
 	      break;
 	  case boss:
-	      fprintf(f, "boss ");
+	      fprintf(f, "boss                ");
 	      break;
 	  case noroute:
-	      fprintf(f, "noroute ");
+	      fprintf(f, "noroute             ");
 	      break;
 	  }
       }
@@ -274,22 +273,22 @@ void dumpMsgArea(s_area *area, char *prefix, FILE *f)
 {
     int i;
 
-    fprintf(f, "%s %s ", prefix, area->areaName);
+    fprintf(f, "%-19s %-39s ", prefix, area->areaName);
 
-    if (area->fileName != NULL) fprintf(f, "%s ", area->fileName);
+    if (area->fileName != NULL) fprintf(f, "%-79s ", area->fileName);
 
     switch (area->msgbType)
     {
     case MSGTYPE_SDM:
-	fprintf(f, "Msg ");
+	fprintf(f, "Msg                 ");
 	break;
 	
     case MSGTYPE_SQUISH:
-	fprintf(f, "Squish ");
+	fprintf(f, "Squish              ");
 	break;
 
     case MSGTYPE_PASSTHROUGH:
-	fprintf(f, "Passthrough ");
+	fprintf(f, "Passthrough         ");
 	break;
     }
 
@@ -346,27 +345,27 @@ void dumpMsgArea(s_area *area, char *prefix, FILE *f)
 
 void dumpMsgAreas(s_fidoconfig *config, FILE *f)
 {
-    int i;
+  int i;
+  
+  dumpMsgArea(&config->netMailArea, "netMailArea        ", f);
+  dumpMsgArea(&config->dupeArea, "dupeArea           ", f);
+  dumpMsgArea(&config->badArea, "badArea            ", f);
 
-    dumpMsgArea(&config->netMailArea, "netMailArea", f);
-    dumpMsgArea(&config->dupeArea, "dupeArea", f);
-    dumpMsgArea(&config->badArea, "badArea", f);
+  fprintf(f, "\n");
 
-    fprintf(f, "\n");
-
-    for (i = 0; i < config->localAreaCount; i++)
-    {
-	dumpMsgArea(&config->localAreas[i], "localArea", f);
-    }
+  for (i = 0; i < config->localAreaCount; i++)
+  {
+    dumpMsgArea(&config->localAreas[i], "localArea          ", f);
+  }
     
-    fprintf(f, "\n");
+  fprintf(f, "\n");
 
-    for (i = 0; i < config->echoAreaCount; i++)
-    {
-	dumpMsgArea(&config->echoAreas[i], "echoArea", f);
-    }
+  for (i = 0; i < config->echoAreaCount; i++)
+  {
+    dumpMsgArea(&config->echoAreas[i], "echoArea           ", f);
+  }
 
-    fprintf(f, "\n");
+  fprintf(f, "\n");
 }
 
 void dumpCarbon(s_carbon *carbon, FILE *f)
@@ -374,25 +373,25 @@ void dumpCarbon(s_carbon *carbon, FILE *f)
     switch (carbon->type)
     {
     case to:
-	fprintf(f, "CarbonTo ");
+	fprintf(f, "CarbonTo            ");
 	break;
     case from:
-	fprintf(f, "CarbonFrom ");
+	fprintf(f, "CarbonFrom          ");
 	break;
     case kludge:
-	fprintf(f, "CarbonKludge ");
+	fprintf(f, "CarbonKludge        ");
 	break;
     case subject:
-	fprintf(f, "CarbonSubj ");
+	fprintf(f, "CarbonSubj          ");
 	break;
     case msgtext:
-	fprintf(f, "CarbonText ");
+	fprintf(f, "CarbonText          ");
 	break;
     }
     
     fprintf(f, "%s\n", carbon->str);
 
-    dumpString(f, "CarbonArea %s\n", carbon->area->areaName);
+    dumpString(f, "CarbonArea          %s\n", carbon->area->areaName);
 }
 
 void dumpCarbons(s_fidoconfig *config, FILE *f)
@@ -404,7 +403,7 @@ void dumpCarbons(s_fidoconfig *config, FILE *f)
 	dumpCarbon(&config->carbons[i], f);
     }
 
-    if (config->carbonAndQuit != 0) fprintf(f, "carbonAndQuit On\n");
+    if (config->carbonAndQuit != 0) fprintf(f, "carbonAndQuit       On\n");
 
     fprintf(f, "\n");
 }
@@ -414,9 +413,9 @@ void dumpAreafix(s_fidoconfig *config, FILE *f)
     if (config->areafixFromPkt != 0) fprintf(f, "areafixFromPkt\n");
     if (config->areafixKillReports != 0) fprintf(f, "areafixKillReports\n");
 
-    dumpString(f, "ReportTo %s\n", config->ReportTo);
+    dumpString(f, "ReportTo            %s\n", config->ReportTo);
 
-    dumpString(f, "PublicGroup %s\n", config->PublicGroup);
+    dumpString(f, "PublicGroup         %s\n", config->PublicGroup);
 
     fprintf(f, "\n");
 }
@@ -427,9 +426,9 @@ void dumpConfig(s_fidoconfig *config, FILE *f)
   dumpPack(config, f);
   dumpPaths(config, f);
   dumpLinks(config, f);
-  dumpRoute(config->route, config->routeCount, "route", f);
-  dumpRoute(config->routeMail, config->routeMailCount, "routeMail", f);
-  dumpRoute(config->routeFile, config->routeFileCount, "routeFile", f);
+  dumpRoute(config->route, config->routeCount, "route              ", f);
+  dumpRoute(config->routeMail, config->routeMailCount, "routeMail          ", f);
+  dumpRoute(config->routeFile, config->routeFileCount, "routeFile          ", f);
   dumpMsgAreas(config, f);
   dumpCarbons(config, f);
   dumpAreafix(config, f);
