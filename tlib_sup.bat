ac712a90847baca5653904c6b562c6c480672b60
tobi <>
2000-04-12 15:05:54 +0000

    legacy makefile things

--- tlib_sup.bat
+++ tlib_sup.bat
@@ -2,13 +2,15 @@
 
 REM This is a binary file - it must have carriage return at the end!
 
-REM This batch file is required for building fidoconfig with Borland C TLIB. 
+REM This batch file is required for building fidoconfig with Borland C TLIB.
 REM It is irrelevant for the end user and for developers with other compilers.
 
-SET LIBNAME=%1
+SET LIBPROG=%1
+SET LIBNAME=%2
 :LOOP
-IF ~%2~ == ~~ GOTO ENDE
-TLIB %LIBNAME% -+%2
+IF ~%3~ == ~~ GOTO ENDE
+echo %LIBPROG% %LIBNAME% -+%3 ,,
+%LIBPROG% %LIBNAME% -+%3 ,,
 SHIFT
 GOTO LOOP
 
