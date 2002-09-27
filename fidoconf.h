48220a3836ab693eb7993dcbc8619ae53b8e5a5d
Max Chernogor <mche@users.sourceforge.net>
2002-09-27 12:25:21 +0000

    correct previous patch

--- fidoconf.h
+++ fidoconf.h
@@ -659,7 +659,13 @@ typedef struct {
     // for future usage
 } sApp;
 
-extern sApp theApp;
+
+
+#ifndef _MAKE_DLL_MVC_
+	extern sApp theApp;
+#else
+	FCONF_EXT sApp theApp;
+#endif
 
 FCONF_EXT void SetAppModule(int mod); // setup struct sApp
 
