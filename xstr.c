23ef61f9c3e24ecae2217c2de8cf17644321e4f7
Max Chernogor <mche@users.sourceforge.net>
2002-10-03 09:31:18 +0000

    INTEL defined for msvc makefiles and vsnprintf fix

--- xstr.c
+++ xstr.c
@@ -31,6 +31,11 @@
 #include "xstr.h"
 #include "common.h"
 
+#ifndef _MAKE_DLL_MVC_
+#  define vsnprintf	_vsnprintf
+#endif
+
+
 #define N_PRINTFBUF     1024
 
 char *xstralloc(char **s, size_t add)
