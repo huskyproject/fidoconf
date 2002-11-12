96c527975bac07d3a68120d01ef7723cf40ce6bf
Stas Degteff <stas_degteff@users.sourceforge.net>
2002-11-12 05:35:01 +0000

    Add top comment

--- cfg.c
+++ cfg.c
@@ -1,3 +1,33 @@
+/* $Id$
+ ******************************************************************************
+ * FIDOCONFIG --- library for fidonet configs
+ ******************************************************************************
+ * Copyright (C) 2000-2002
+ *
+ * Max Levenkov
+ * Husky development team
+ * http://husky.sourceforge.net/team/html
+ *
+ * This file is part of FIDOCONFIG.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; see file COPYING. If not, write to the Free
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * See also http://www.gnu.org
+ *****************************************************************************
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
