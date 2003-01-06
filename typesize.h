32236f3802ca41adfd401573dd891f343df3c927
Volker Quetschke <quetschke@users.sourceforge.net>
2003-01-06 15:57:22 +0000

    Re-added __CYGWIN__ to the list of other unix like systems. This got lost between rev. 1.23 and 1.24 by the patch of mche.
    Cygwin uses gcc and *really* needs theese type definitions. (2:2411/12.99)

--- typesize.h
+++ typesize.h
@@ -102,7 +102,7 @@ typedef          void       VOID;
 #endif
 #endif                                          /*  #if defined(__EMX__) */
 
-#if (defined(__linux__) && !defined(__alpha)) || defined(__FreeBSD__) || defined(__DJGPP__) || defined(__BEOS__) || defined(__OpenBSD__)
+#if (defined(__linux__) && !defined(__alpha)) || defined(__FreeBSD__) || defined(__DJGPP__) || defined(__BEOS__) || defined(__OpenBSD__) || defined(__CYGWIN__)
 typedef   signed char       CHAR;               /*  1 byte */
 typedef unsigned char      UCHAR;               /*  1 byte */
 typedef   signed int        INT32;              /*  4 byte */
