33b6876ad5d95cefe734d761de94c1c58a8ebaa5
Stas Degteff <stas_degteff@users.sourceforge.net>
2003-02-14 15:29:13 +0000

    Update version check to new -current

--- tparser.c
+++ tparser.c
@@ -1160,15 +1160,15 @@ int main(int argc, char **argv) {
 				FC_VER_PATCH, FC_VER_BRANCH, cvs_date ));
    nfree(module); /* used as a temporary variable */
 
-   if( !CheckFidoconfigVersion(1,3,0,BRANCH_CURRENT,cvs_date) ) {
-     printf("Incompatible version of FIDOCONFIG library: require fidoconfig-1.3.0-current at %s\n",cvs_date);
+   if( !CheckFidoconfigVersion(1,9,0,BRANCH_CURRENT,cvs_date) ) {
+     printf("Incompatible version of FIDOCONFIG library: require fidoconfig-1.9.0-current at %s\n",cvs_date);
      exit(255);
    }
-   if( !CheckSmapiVersion(2,3,0,smapi_cvs_date()) ) {
-     printf("Incompatible version of SMAPI: require smapi-2.3.0-current at %s\n",smapi_cvs_date());
+   if( !CheckSmapiVersion(2,5,0,smapi_cvs_date()) ) {
+     printf("Incompatible version of SMAPI: require smapi-2.5.0-current at %s\n",smapi_cvs_date());
      exit(255);
    }
-   printf("using smapi-2.3.0-current at %s and fidoconfig-1.3.0-current at %s\n\n",smapi_cvs_date(),cvs_date);
+   printf("using smapi-2.5.0-current at %s and fidoconfig-1.9.0-current at %s\n\n",smapi_cvs_date(),cvs_date);
 
    for (k=1; k<argc; k++)
    {
