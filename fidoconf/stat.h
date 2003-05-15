8ef2959dd2dc030ec4d41dd2652b1e4775e151c5
Max Chernogor <mche@users.sourceforge.net>
2003-05-15 14:25:34 +0000

    fix windows build

--- fidoconf/stat.h
+++ fidoconf/stat.h
@@ -1,12 +1,14 @@
 #ifndef _STAT_H
 #define _STAT_H
 
+#include "fidoconf.h"
+
 #define ADV_STAT
 
 /* stat record type */
 typedef enum { stNORM, stBAD, stDUPE, stOUT } st_type;
 
-void put_stat(s_area *echo, hs_addr *link, st_type type, long len);
-void upd_stat(char *file);
+HUSKYEXT void put_stat(s_area *echo, hs_addr *link, st_type type, long len);
+HUSKYEXT void upd_stat(char *file);
 
 #endif
