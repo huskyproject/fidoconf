3038e10b10680d7f42c63d63667191b145e76c68
sfpavel <>
2004-03-15 15:32:58 +0000

    fix parsing AutoCreateDefaults (patch by mche)

--- common.c
+++ common.c
@@ -92,7 +92,7 @@ int copyString(char *str, char **pmem)
    }
 
    nfree(*pmem);
-   *pmem = (char *) sstrdup (stripRoundingChars(str, "\""));
+   *pmem = (char *) sstrdup (stripRoundingChars(str, "\n"));
 
    return 0;
 }
