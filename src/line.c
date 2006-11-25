cc54d0151cb3d6238772716f756a05023a8be464
Nick Voronin <nick_voronin@users.sourceforge.net>
2006-11-25 13:29:47 +0000

    remove duplicated code

--- src/line.c
+++ src/line.c
@@ -2150,13 +2150,6 @@ int parsePack(char *line, s_fidoconfig *config) {
        return 2;
    }
 
-   /* check for no link definition was before */
-   if(config->linkCount > 0)
-   {
-       prErr("Unable to add commandline for packer after link definition!");
-       return 2;
-   }
-
    p = strtok(line, " \t");
    c = getRestOfLine();
    if ((p != NULL) && (c != NULL)) {
