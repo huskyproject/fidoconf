6787d394c6ea2309deed82bb872514a1a2161bc3
Stas Degteff <stas_degteff@users.sourceforge.net>
2010-02-03 17:27:14 +0000

    Update licension info

--- src/tparser.c
+++ src/tparser.c
@@ -27,6 +27,7 @@
  * You should have received a copy of the GNU Library General Public
  * License along with this library; see file COPYING. If not, write to the Free
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or download licence from http://www.gnu.org.
  *****************************************************************************
  *   $Id$
  */
